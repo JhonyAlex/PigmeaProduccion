1433cd7ec937b0d8e260b666827ff421a35c9c63
JhonyAlex <39017484+JhonyAlex@users.noreply.github.com>
2025-03-21 07:44:48 +0000

    Limpieza de código

--- resources/views/maquinas/index.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- En la sección de scripts -->
-@section('scripts')
-    <!-- Otros scripts -->
-    <script src="{{ asset('js/maquinas.js') }}"></script>
-    <script src="{{ asset('js/maquinas-debug.js') }}"></script>
-@endsection
